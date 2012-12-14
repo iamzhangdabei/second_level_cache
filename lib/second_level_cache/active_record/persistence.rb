a5268704e272d5503be9a8a3c078ecdd978ec862
zhangdabei <keepyouliking@gmail.com>
2012-12-14 14:12:39 +0800

    clear dependency to fit rails3.0
    clear dependency to fit rails3.0

--- lib/second_level_cache/active_record/persistence.rb
+++ lib/second_level_cache/active_record/persistence.rb
@@ -8,13 +8,13 @@ module SecondLevelCache
         class_eval do
           alias_method_chain :reload, :second_level_cache
           alias_method_chain :touch, :second_level_cache
-          alias_method_chain :update_column, :second_level_cache
+          #alias_method_chain :update_column, :second_level_cache
         end
       end
 
-      def update_column_with_second_level_cache(name, value)
-        update_column_without_second_level_cache(name, value).tap{update_second_level_cache}
-      end
+      # def update_column_with_second_level_cache(name, value)
+      #  update_column_without_second_level_cache(name, value).tap{update_second_level_cache}
+      # end
 
       def reload_with_second_level_cache(options = nil)
         reload_without_second_level_cache(options).tap{expire_second_level_cache}
